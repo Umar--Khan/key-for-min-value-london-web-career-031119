c50fb0e194b6d65d5fc37ebbe6d3a808ab7e4412
Umar Khan <umarkhandev@gmail.com>
2019-03-07 00:08:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,6 @@ def key_for_min_value(name_hash)
 low_key = nil
 low_value = nil
 name_hash.each do |key, value|
-  # if value < lowest_value
     if low_value == nil || value < low_value
        low_value = value
        low_key = key
